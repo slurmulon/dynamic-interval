f0a0871508d2946f3923eae5664dda3936805d87
evavro <me@madhax.io>
2017-05-14 21:15:49 -0700

    :package: dist update

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 // http://stackoverflow.com/a/7445863
+
+/**
+ * @param {Function} next method that returns the interval gap for the next tick
+ * @param {Number} init initial interval gap
+ * @returns {Function}
+ */
 var setDynterval = exports.setDynterval = function setDynterval(next, init) {
   var context = { wait: init };
 
   var step = function step() {
     clearInterval(interval);
 
-    context.wait = next(context);
+    context.wait = next(context) || context.wait;
 
     interval = setInterval(step, context.wait);
   };
 
-  var interval = setInterval(step, context.tick);
+  var interval = setInterval(step, context.wait);
 
   return interval;
 };
-
-// TEST
-
-setDynterval(function (interval) {
-  console.log('interval', interval);return interval.wait * 2;
-}, 50);
