22b7cef61ff4690c49c15f758f8938df554cc9a3
evavro <me@madhax.io>
2017-08-05 00:36:42 -0700

    :sparkles: clean up (removed old impl)

--- dist/index.js
+++ dist/index.js
@@ -10,25 +10,6 @@ Object.defineProperty(exports, "__esModule", {
  * @param {Number} init initial interval gap
  * @returns {Function}
  */
-// TODO: allow this to support a proper context objec tthat allows users to piggy
-// back state through each tick
-// export const setDynterval = (next, wait, ctx = {}) => {
-//   let context = Object.assign(ctx, { wait })
-
-//   const step = () => {
-//     clearInterval(interval)
-
-//     // context.wait = next(context).wait || context.wait
-//     context = next(context) || context
-
-//     interval = setInterval(step, context.wait)
-//   }
-
-//   let interval = setInterval(step, context.wait)
-
-//   return interval
-// }
-
 var setDynterval = exports.setDynterval = function setDynterval(next, init) {
   var context = Object.assign({ wait: 0 }, init);
 
--- index.js
+++ index.js
@@ -5,25 +5,6 @@
  * @param {Number} init initial interval gap
  * @returns {Function}
  */
-// TODO: allow this to support a proper context objec tthat allows users to piggy
-// back state through each tick
-// export const setDynterval = (next, wait, ctx = {}) => {
-//   let context = Object.assign(ctx, { wait })
-
-//   const step = () => {
-//     clearInterval(interval)
-
-//     // context.wait = next(context).wait || context.wait
-//     context = next(context) || context
-
-//     interval = setInterval(step, context.wait)
-//   }
-
-//   let interval = setInterval(step, context.wait)
-
-//   return interval
-// }
-
 export const setDynterval = (next, init) => {
   let context = Object.assign({ wait: 0 }, init)
 
